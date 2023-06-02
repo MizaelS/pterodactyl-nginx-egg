cc42903475ccfa0494387addf4579b11142f4bdc
MizaelS <92831643+MizaelS@users.noreply.github.com>
2023-06-02 06:27:58 -0600

    Validación para versión de nginx

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@ rm -rf /home/container/tmp/*
 
 echo -e "\033[32m⟳\033[0m Iniciando PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+echo -e "\033[32m⟳\033[0m Validando versión de PHP..."
+php -v
 
 echo -e "\033[32m⟳\033[0m Iniciando Nginx..."
-echo -e "\033[32m⟳\033[0m Validando versión de php..."
-php -v
+echo -e "\033[32m⟳\033[0m Validando versión de Nginx..."
+nginx -v
 echo -e "\033[32m✓\033[0m Servidor web iniciado con éxito"
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
