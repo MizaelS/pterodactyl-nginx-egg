53ce3adb29de9d62ac48405f2dc112b009340014
MizaelS <92831643+MizaelS@users.noreply.github.com>
2023-06-01 18:00:42 -0600

    Se añadió la validación para la versión de php

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ echo -e "\033[32m⟳\033[0m Iniciando PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo -e "\033[32m⟳\033[0m Iniciando Nginx..."
+echo -e "\033[32m⟳\033[0m Validando versión de php..."
+php -v
 echo -e "\033[32m✓\033[0m Servidor web iniciado con éxito"
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
